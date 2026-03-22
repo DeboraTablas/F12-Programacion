9f612c1545430243db6f2e99d1780e8026cd98c9
Debora Tablas <esmeral.cin@gmail.com>
2026-03-21 18:40:07 -0600

    agrega

---
+++ autograder/tareas/numero_primo/solucion4.cpp
@@ -0,0 +1,26 @@
+ #include <cmath> y sqrt(n)
+ #include <iostream>
+
+ 
+using namespace std;
+
+int es_primo(int n) {
+    for (int d = 2; d * d <= n; d++) {
+        if (n % d == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (es_primo(n))
+        cout << "primo";
+    else
+        cout << "no primo";
+
+    return 0;
+}
